43c4fe49dbbfc9c9edc4c801bfec145cc4972df6
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-16 18:31:38 -0800

    Add print_board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,17 @@
+# Board Class
 class Board
   attr_accessor :spaces
 
   def start
     self.spaces = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  end
+
+  def print_board
+    spaces.each do |row|
+      puts ''
+      puts "#{row[0]} | #{row[1]} | #{row[2]}"
+      print '---------' unless row == spaces[2]
+      puts '' if row == spaces[2]
+    end
   end
 end
