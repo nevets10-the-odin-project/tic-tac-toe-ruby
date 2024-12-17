fbadd8f19956ffa82f3d1099c10047ba588c9c41
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-16 21:03:07 -0800

    Refactor win logic, print board at end of game
    I broke up the conditions into a winner and a tie so they can be tracked
     independently.

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,12 @@
 # Board Class
 class Board
-  attr_accessor :spaces, :current_player_index, :is_game_end
+  attr_accessor :spaces, :current_player_index
 
   WIN_INDEXES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
   def start
     self.spaces = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     self.current_player_index = 1
-    self.is_game_end = false
   end
 
   def update_board(choice, player_token)
@@ -29,19 +28,19 @@ class Board
     self.current_player_index = current_player_index.zero? ? 1 : 0
   end
 
-  def game_end?(token)
+  def winner?(token)
     flat_spaces = spaces.flatten
+    is_winner = false
 
     WIN_INDEXES.each do |win_line|
       line_check = [flat_spaces[win_line[0]], flat_spaces[win_line[1]], flat_spaces[win_line[2]]]
       if line_check.all?(token)
-        self.is_game_end = true
-        break
-      elsif flat_spaces.all? { |space| space.is_a?(String) }
-        self.is_game_end = true
+        is_winner = true
         break
       end
     end
+
+    is_winner
   end
 
   def tie?
