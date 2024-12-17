021115934f8f802c2e493170468908fee29edfb8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-16 19:31:59 -0800

    Put players into an array

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,16 +2,15 @@ require_relative('lib/board')
 require_relative('lib/player')
 
 board = Board.new
-player1 = Player.new('X')
-player2 = Player.new('O')
+players = [Player.new('X'), Player.new('O')]
 
 puts "What is player 1's name?"
-player1.name = gets.chomp
+players[0].name = gets.chomp
 
 puts "What is player 2's name?"
-player2.name = gets.chomp
+players[1].name = gets.chomp
 
-puts "Okay, #{player1.name} and #{player2.name}, let's play Tic-Tac-Toe!"
+puts "Okay, #{players[0].name} and #{players[1].name}, let's play Tic-Tac-Toe!"
 
 board.start
 board.print_board
